79808475ef62d3ac64a74fb1fcc53d68014d64d0
TheBeff <thebeff@gmail.com>
2016-07-19 20:38:17 -0400

    Configured Express

--- app.js
+++ app.js
@@ -3,6 +3,16 @@ var app = express();
 
 app.listen(3000);
 
+app.use(function (req, res, next) {
+   console.log(req.method + " " + req.originalUrl + " " + res.statusCode);
+   next();
+})
+
+app.use('/special/', function (req, res, next) {
+   console.log("You reached the special area.");
+   next();
+})
+
 app.get('/', function(req, res){
 	res.send("WELCOME, N00B.");
 });
